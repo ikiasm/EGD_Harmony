f5e7b7b70b5d4882b75eed578884b4845296830e
marcelo <marcelo@192.168.1.14>
2018-12-03 17:13:56 -0300

    void secuenciaIny(avance, tiempoIny) Funcion que recibe avance (en grados) y tiempoIny en (microsegundos) desde source de comunicacion Requiere para lanzar la secuencia una señal de sincronismo (sincroFlag) que la levanta el source de deteccion de dientes y la baja la misma secuenciaIny. Una vez implementada deteccion de dientes: -Remover int rpm de secuenciaIny -Remover static bool sincroFlag de las variable globales de app.c Una vez implementada comunicaciones: -Remover static int avance y static int tiempoIny de las variables globales de app.c

--- firmware/src/system_config/default/system_init.c
+++ firmware/src/system_config/default/system_init.c
@@ -100,46 +100,6 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 // *****************************************************************************
 // <editor-fold defaultstate="collapsed" desc="DRV_USART Initialization Data">
-
-const DRV_USART_INIT drvUsart0InitData =
-{
-    .moduleInit.value = DRV_USART_POWER_STATE_IDX0,
-    .usartID = DRV_USART_PERIPHERAL_ID_IDX0, 
-    .mode = DRV_USART_OPER_MODE_IDX0,
-    .flags = DRV_USART_INIT_FLAGS_IDX0,
-    .brgClock = DRV_USART_BRG_CLOCK_IDX0,
-    .lineControl = DRV_USART_LINE_CNTRL_IDX0,
-    .baud = DRV_USART_BAUD_RATE_IDX0,
-    .handshake = DRV_USART_HANDSHAKE_MODE_IDX0,
-    .linesEnable = DRV_USART_LINES_ENABLE_IDX0,
-    .interruptTransmit = DRV_USART_XMIT_INT_SRC_IDX0,
-    .interruptReceive = DRV_USART_RCV_INT_SRC_IDX0,
-    .interruptError = DRV_USART_ERR_INT_SRC_IDX0,
-    .dmaChannelTransmit = DMA_CHANNEL_NONE,
-    .dmaInterruptTransmit = DRV_USART_XMIT_INT_SRC_IDX0,    
-    .dmaChannelReceive = DMA_CHANNEL_NONE,
-    .dmaInterruptReceive = DRV_USART_RCV_INT_SRC_IDX0,    
-};
-
-const DRV_USART_INIT drvUsart1InitData =
-{
-    .moduleInit.value = DRV_USART_POWER_STATE_IDX1,
-    .usartID = DRV_USART_PERIPHERAL_ID_IDX1, 
-    .mode = DRV_USART_OPER_MODE_IDX1,
-    .flags = DRV_USART_INIT_FLAGS_IDX1,
-    .brgClock = DRV_USART_BRG_CLOCK_IDX1,
-    .lineControl = DRV_USART_LINE_CNTRL_IDX1,
-    .baud = DRV_USART_BAUD_RATE_IDX1,
-    .handshake = DRV_USART_HANDSHAKE_MODE_IDX1,
-    .linesEnable = DRV_USART_LINES_ENABLE_IDX1,
-    .interruptTransmit = DRV_USART_XMIT_INT_SRC_IDX1,
-    .interruptReceive = DRV_USART_RCV_INT_SRC_IDX1,
-    .interruptError = DRV_USART_ERR_INT_SRC_IDX1,
-    .dmaChannelTransmit = DMA_CHANNEL_NONE,
-    .dmaInterruptTransmit = DRV_USART_XMIT_INT_SRC_IDX1,
-    .dmaChannelReceive = DMA_CHANNEL_NONE,
-    .dmaInterruptReceive= DRV_USART_RCV_INT_SRC_IDX1,
-};
 // </editor-fold>
 
 // *****************************************************************************
@@ -199,8 +159,8 @@ void SYS_Initialize ( void* data )
     /*Initialize TMR2 */
     DRV_TMR2_Initialize();
  
-     sysObj.drvUsart0 = DRV_USART_Initialize(DRV_USART_INDEX_0, (SYS_MODULE_INIT *)&drvUsart0InitData);
-    sysObj.drvUsart1 = DRV_USART_Initialize(DRV_USART_INDEX_1, (SYS_MODULE_INIT *)&drvUsart1InitData);
+     sysObj.drvUsart0 = DRV_USART_Initialize(DRV_USART_INDEX_0, (SYS_MODULE_INIT *)NULL);
+    sysObj.drvUsart1 = DRV_USART_Initialize(DRV_USART_INDEX_1, (SYS_MODULE_INIT *)NULL);
     SYS_INT_VectorPrioritySet(INT_VECTOR_UART5, INT_PRIORITY_LEVEL1);
     SYS_INT_VectorSubprioritySet(INT_VECTOR_UART5, INT_SUBPRIORITY_LEVEL0);
     SYS_INT_VectorPrioritySet(INT_VECTOR_UART2, INT_PRIORITY_LEVEL1);
